100ab4973f809debf775f155a368dd9fac858146
SergoShe <snow5373@gmail.com>
2022-04-12 12:39:59 +0700

    Task2 ver.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Task2.evenAndOdd(-1579462);
+        System.out.println(Task2.summa(-497527564));
+        System.out.println(Task2.reverseNum(890877424));
     }
 }
