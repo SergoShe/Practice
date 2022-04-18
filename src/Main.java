28af9da8889ae75cbdcdefbdcf30bd4d1c01681f
SergoShe <snow5373@gmail.com>
2022-04-18 17:53:32 +0700

    minor comments corrected

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void StringProcessing1(String text) {
         int len = text.length();
         if (len > 5) {
-            System.out.println(text.substring(0,3) + text.substring(len-3));
+            System.out.println(text.substring(0, 3) + text.substring(len - 3));
         } else {
-            System.out.println(text.substring(0,1).repeat(len));
+            System.out.println(text.substring(0, 1).repeat(len));
         }
     }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(Task3.fillZerosAndOnes(10)));
         System.out.println(Arrays.toString(Task3.fillOdds(10)));
--- src/Task3.java
+++ src/Task3.java
@@ -2,42 +2,44 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Task3 {
-    public static int[] fillZerosAndOnes (int num) {
+    public static int[] fillZerosAndOnes(int num) {
         int[] array = new int[num];
-        for (int i = 0;i<num;i++) {
-            if (i%2==1)
-                array[i]=1;
+        for (int i = 1; i < num; i += 2) {
+            array[i] = 1;
         }
         return array;
     }
-    public static int[] fillOdds (int num) {
+
+    public static int[] fillOdds(int num) {
         int[] array = new int[num];
-        for (int i = 0;i<num;i++) {
-            array[i]=1+2*i;
+        for (int i = 0; i < num; i++) {
+            array[i] = 1 + 2 * i;
         }
         return array;
     }
-    public static int[] fillRandomZerosAndOnes (int num) {
+
+    public static int[] fillRandomZerosAndOnes(int num) {
         int[] array = new int[num];
         Random random = new Random();
-        Arrays.fill(array,1);
-        int numberOfZeros = random.nextInt(num/4+1);
+        Arrays.fill(array, 1);
+        int numberOfZeros = random.nextInt(num / 4 + 1);
         int countZero = 0;
-        while ( countZero < numberOfZeros) {
+        while (countZero < numberOfZeros) {
             int randNum = random.nextInt(num);
             if (array[randNum] == 1) {
-                array[randNum]=0;
+                array[randNum] = 0;
                 countZero++;
             }
         }
         return array;
     }
-    public static void fillRandomTwoDimensionalArray (int num) {
+
+    public static void fillRandomTwoDimensionalArray(int num) {
         int[][] array = new int[num][num];
         int sum = 0;
         Random random = new Random();
-        for (int i = 0; i<num;i++){
-            for (int j = 0; j<num;j++) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
                 array[i][j] = random.nextInt();
                 sum += array[i][j];
             }
